2b972db28f295688982f65cec5322cc03478d16c
Tú (Scarcy Mã) <ngoctutu1102@gmail.com>
2019-12-21 23:30:39 +0700

    Update

---
+++ Presentation document/Soucre Code/Appium/Calculator.java
@@ -0,0 +1,44 @@
+package Appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Calculator{
+	@Test
+	public void plus100() throws MalformedURLException {
+		//Thiết lập môi trường
+		//1. Lấy tên device, platform, appPackage, appActivity
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5556");
+		dc.setCapability("platformName", "android");
+		dc.setCapability("appPackage", "com.android.calculator2");
+		dc.setCapability("appActivity", ".Calculator");
+		//Đường dẫn liên kết đến Appium
+		AndroidDriver<AndroidElement> wdr = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+		
+		//Thực hiện chạy automation test
+		MobileElement el1 = (MobileElement) wdr.findElementById("com.android.calculator2:id/digit_7");
+		el1.click();
+		MobileElement el2 = (MobileElement) wdr.findElementById("com.android.calculator2:id/digit_5");
+		el2.click();
+		MobileElement el3 = (MobileElement) wdr.findElementByAccessibilityId("plus");
+		el3.click();
+		MobileElement el4 = (MobileElement) wdr.findElementById("com.android.calculator2:id/digit_2");
+		el4.click();
+		MobileElement el5 = (MobileElement) wdr.findElementById("com.android.calculator2:id/digit_5");
+		el5.click();
+		MobileElement el6 = (MobileElement) wdr.findElementByAccessibilityId("equals");
+		el6.click();
+		//Hiển thị kết quả
+		Assert.assertEquals(wdr.findElementById("com.android.calculator2:id/result").getText(),"100");
+	}
+}
